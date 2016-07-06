8673e144e94e871d38b688388897293db20f68c4
javaday <javaday@gmail.com>
2016-07-06 11:28:35 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+(function () {
+
+	$('#addCityBtn').on('click', function () {
+
+		var zip = $('#newZipCode').val();
+
+		// get the city & weather data
+		// add a city panel to the list		
+	});
+
+
+	function getCity(zip, callback) {
+		
+		var urlBase = 'http://api.zippopotam.us/us/';
+		var url = urlBase + zip;
+
+		$.get(url, function (data, status) {
+
+			// transform the data to camelCase proprty names			
+		});
+	}
+
+	function getWeather(zip, callback) {
+
+		var urlBase = 'http://api.openweathermap.org/data/2.5/';
+		var appId = 'bd82255fd0a21fa1238699b9eda2ee35';
+		var url = urlBase + 'weather?appid=' + appId + '&units=imperial&zip=' + zip;
+
+		$.get(url, function (data, status) {
+			
+		});
+	}
+})();
